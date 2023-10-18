c22fd2fdc0311be37795185f6e6bb64c599b965d
khardy <khardy.official@gmail.com>
2023-10-18 13:45:25 +0300

    fix: button language

--- src/redux/reducers/cartSlice.ts
+++ src/redux/reducers/cartSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 import type { CartItemProps } from '../../../src/Containers/Cart/CartNotEmpty/CartItem/CartItem';
 
